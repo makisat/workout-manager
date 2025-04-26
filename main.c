c11d2ca66cfbf922483dab8b8dbd789d60e28af8
makisat <makiya28sato@gmail.com>
2025-04-26 01:49:28 -0400

    addSet function added

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 typedef struct {
   char* year, month, day;
@@ -6,10 +8,11 @@ typedef struct {
 } Date;
 
 typedef struct {
-  char* username;
-  Workout* workouts;
+  char *name;
+  uint8_t set_length;
+  uint8_t* sets_reps;
 
-} User;
+} Exercise;
 
 typedef struct {
   Date date;
@@ -18,17 +21,62 @@ typedef struct {
 } Workout;
 
 typedef struct {
-  char* name;
-  unsigned char sets;
-  unsigned char reps;
+  char* username;
+  Workout* workouts;
 
-} Exercise;
+} User;
 
+User createUser(char *username) {
+  return (User) {.username = username};
+}
 
+void addWorkout(User *user, Workout workout) {}
+
+void addExercise(Workout *workout, Exercise exercise) {}
+
+int addSet(Exercise *exercise, uint8_t reps) {
+  if (exercise->set_length == 0) {
+    exercise->set_length = 1;
+    exercise->sets_reps = (uint8_t *)malloc(sizeof(uint8_t));
+    if (exercise->sets_reps == NULL) {
+      printf("memory allocation failed\n");
+      return 1;
+    }
+    exercise->sets_reps[0] = reps;
+    return 0;
+  }
+
+  exercise->set_length++;
+  uint8_t* temp_ptr = (uint8_t*) realloc(exercise->sets_reps, sizeof(uint8_t) * exercise->set_length);
+  if (exercise->sets_reps == NULL) {
+    printf("memory allocation failed\n");
+    exercise->set_length--;
+    return 1;
+  }
+  exercise->sets_reps = temp_ptr;
+  exercise->sets_reps[exercise->set_length - 1] = reps;
+  return 0;
+}
 
 int main() {
 
+  Exercise exe = {.name = "test"};
 
+  if (addSet(&exe, 10) == 1) {
+    printf("failed add set\n");
+    return 1;
+  }
+
+  if (addSet(&exe, 11) == 1) {
+    printf("failed add set\n");
+    return 1;
+  }
+
+  for (int i = 0; i < exe.set_length; i++) {
+    printf("%d\n", exe.sets_reps[i]);
+  }
+
+  free(exe.sets_reps);
 
   return 0;
 }
